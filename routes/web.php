cbb21273cb7d7d1f5e4b1fe3cc7e6c8f5695da51
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-10-02 23:14:16 +0100

    e-shop-laravel-adding-lli

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', HomeController::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
